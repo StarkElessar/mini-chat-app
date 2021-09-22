e54f87d9aa8981c7881b04274aeb7a486edc78de
Stark <serj.elessar@gmail.com>
2021-09-22 10:17:30 +0300

    Destructuring data in a folder LogIn

--- src/components/LogIn/index.jsx
+++ src/components/LogIn/index.jsx
@@ -27,15 +27,15 @@ const LogIn = () => {
   return (
     <div className='login__wrapper'>
       <form action="#" method='GET'>
-        <div className='labelInput'>
+        <div className='label__input'>
           <label htmlFor="email">Email</label>
           <input value={email} onChange={onEmailChange} type="text" id='email' name='user_email' placeholder='Enter Your Email' required />
         </div>
-        <div className='labelInput'>
+        <div className='label__input'>
           <label htmlFor="pass">Password</label>
           <input value={password} onChange={onPasswordChange} type="password" id='pass' name='user_pass' placeholder='Enter Your Password' required />
         </div>
-        <div className='labelInput'>
+        <div className='label__input'>
           <NavLink to='/main-page'>
             <button disabled={!email || !password} onClick={authenticateUser}>
               Login
